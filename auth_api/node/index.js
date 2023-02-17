90639f104794075eef52cc28d9ae5f187621c565
olatokunbo <olatokunboalege@gmail.com>
2023-02-17 17:42:25 +0100

    fixxed some bugs

--- auth_api/node/index.js
+++ auth_api/node/index.js
@@ -1,8 +1,6 @@
-import Config from 'config';
 import app from './server';
 
-let config = Config;
 
- app.listen(config.port, function() {
-  console.log('listening at',config.port);
+ app.listen(process.env.APP_PORT, function() {
+  console.log('listening at',process.env.APP_PORT);
 });
--- auth_api/node/routes/login.js
+++ auth_api/node/routes/login.js
@@ -3,11 +3,10 @@ import { loginFunction } from '../services/login';
 export const login = async (req, res, next) => {
   let username = req.body.username;
   let password = req.body.password;
-
+ 
   let response = {
     "data": await loginFunction(username, password)
   };
   res.send(response);
   next();
 }
-
